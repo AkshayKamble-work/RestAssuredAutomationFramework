4fee3b624a134400df0488568c27634f802b0105
akshay <Akshaydkamble.01@gmail.com>
2025-03-17 20:14:09 +0530

    Framework part 1

--- src/test/java/TestsCases/CRUD/Test_HealthCheck.java
+++ src/test/java/TestsCases/CRUD/Test_HealthCheck.java
@@ -1,4 +1,25 @@
 package TestsCases.CRUD;
 
-public class Test_HealthCheck {
+import Base.BaseTest;
+import Endpoints.APIConstants;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.TmsLink;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class Test_HealthCheck extends BaseTest {
+
+    @Test(groups = "reg", priority = 1)
+    @TmsLink("https://bugz.atlassian.net/browse/TS-1")
+    @Owner("Akshay")
+    @Description("TC#3  - Verify Health")
+    public void testGETHealthCheck() {
+        requestSpecification.basePath(APIConstants.PING_URL);
+        response = RestAssured.given(requestSpecification)
+                .when()
+                .get();
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(201);
+    }
 }
